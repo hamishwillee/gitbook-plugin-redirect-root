be8d0733dd3e957c8147ed379724044b49c88ddd
Hamish Willee <hamishwillee@gmail.com>
2017-05-01 14:31:34 +1000

    update basic tests

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ module.exports = {
         // This is called after the book generation
         "finish": function() {
             console.log("finish!");
+            console.log("RUNON");
+            var redirectConf = this.config.get("pluginsConfig.language");
+            console.log("REDIRLANG", redirectConf);
         }
     }
 };
+
